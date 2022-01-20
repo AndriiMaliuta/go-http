fef0f4428a7eab1bad28db9e6596e1707ab57dac
Andrii Maliuta <quadr988@gmail.com>
2022-01-20 17:20:06 +0200

    init

---
+++ func/func.go
@@ -0,0 +1,7 @@
+package _func
+
+import "net/http"
+
+func CustHandle() {
+	http.Handle("", nil)
+}
---
+++ models/models.go
@@ -0,0 +1,12 @@
+package models
+
+type Cat struct {
+	Name  string
+	Age   int
+	Color string
+}
+
+type City struct {
+	Name       string
+	Population int64
+}
